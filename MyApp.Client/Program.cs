306a4562794a88a887d8158986ebed925f40ffaf
Olrik van Dijk <olrik.vandijk@ek-robotics.com>
2021-08-10 19:36:41 +0200

    Added Azure AD Open ID authentication

--- MyApp.Client/Program.cs
+++ MyApp.Client/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Fluxor;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyApp.Client.Services;
 
@@ -24,6 +25,13 @@ namespace MyApp.Client
 
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMsalAuthentication(options => 
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/openid");
+            });
+
             builder.Services.AddSingleton<IVehicleService, VehicleService>();
 
             await builder.Build().RunAsync();
